afa375cc8e8d41fb11880d05b6aaae42c90961aa
Prata Neaupane <sereversion@gmail.com>
2020-06-05 22:37:37 +0545

    [update  23:48]- video part 11- before setting db and secret key bash_profile

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '2d4fa431becc6318435b149f7649e37f'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
